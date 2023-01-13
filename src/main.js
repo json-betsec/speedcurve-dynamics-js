0845b1a87cfcf0a0544214d1692e1e526508853a
Jason McKenzie <jason.mckenzie@betsecure.com>
2023-01-13 16:20:26 +1000

    Assume repo in same directory

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 console.log(process.env);
-const exports = await import(
-  `${process.env.GITHUB_WORKSPACE}/.github/speedcurve-patching/patch-transaction`
-);
+const exports = await import(".github/speedcurve-patching/patch-transaction");
 console.log("Hello World!", exports);
